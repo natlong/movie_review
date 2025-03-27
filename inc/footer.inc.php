5caa0f7402f1f0a9e11c0ddbc2464000d2f2fc86
natlong <2401643@sit.singaporetech.edu.sg>
2025-03-27 22:10:21 +0800

    Add php files

---
+++ inc/footer.inc.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+          <!-- Footer Links -->
+    <div class="footer-links">
+        <a href="aboutUs.html" class="footer-link">About Us</a>
+        <a href="request.html" class="footer-link">Request a Movie</a>
+        <a href="support.html" class="footer-link">Support</a>
+    </div>
+  
+    <!-- Copyright Section -->
+    <div class="footer-bottom">
+        <p>&copy; 2025 MovieVerse | All Rights Reserved</p>
+    </div>
+</footer>
